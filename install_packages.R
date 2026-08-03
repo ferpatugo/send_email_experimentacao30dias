479cf5578d4212ca18288ac1b3e939163f3da14e
ferpatugo <t_hiagomarques@hotmail.com>
2026-08-03 16:50:57 -0300

    Fix formatting in install_packages.R

--- install_packages.R
+++ install_packages.R
@@ -1,6 +1,6 @@
 pkgs <- c(
   "plumber", "DBI", "RPostgres", "jsonlite",
-  "blastula", "glue", "httr2"
+  "blastula", "glue", "httr2" 
 )
 install.packages(pkgs, repos = "https://cloud.r-project.org")
 
